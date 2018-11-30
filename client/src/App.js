352a2681a48f462e1205232b241fcaaa5446a85c
Matthew Wright <matthewbwright@gmail.com>
2018-11-30 11:27:26 -0700

    creates routes in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Switch, NavLink, withRouter } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import DadJokes from './components/DadJokes';
+
 import './App.css';
 
 class App extends Component {
   render() {
+    const token = localStorage.getItem('token');
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <nav>
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/register">REGISTER</NavLink>
+          <NavLink to="/login">LOGIN</NavLink>
+        </nav>
+        <Switch>
+          <Route exact path="/" render={props => <DadJokes {...props} />} />
+          <Route path="/register" render={props => <Register {...props} />} />
+          <Route path="login" render={props => <Login {...props} />} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
